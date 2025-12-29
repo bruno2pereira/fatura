2a8cac92644f35420a63e33bc807e9aea625a755
brunopereira <bruno.cpereira@prozis.com>
2025-12-29 20:22:25 +0000

    feat: Make toolbar title clickable to navigate to the home page.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,7 +2,7 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-toolbar-title :class="$q.screen.lt.sm ? 'text-subtitle1' : ''">NeonRetreat APP</q-toolbar-title>
+        <q-toolbar-title :class="$q.screen.lt.sm ? 'text-subtitle1' : ''" @click="router.push('/')">NeonRetreat APP</q-toolbar-title>
         <div v-if="$q.screen.gt.xs" class="text-caption">v1.0</div>
       </q-toolbar>
     </q-header>
@@ -14,4 +14,7 @@
 </template>
 
 <script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
 </script>
